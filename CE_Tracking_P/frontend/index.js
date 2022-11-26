9467a948bd280ac592c641547b485315ebdb777f
Franco Sagot - SKG Art <60370730+skgdraws@users.noreply.github.com>
2022-11-25 23:47:20 -0600

    el la selección del mapa funciona correctamente

--- CE_Tracking_P/frontend/index.js
+++ CE_Tracking_P/frontend/index.js
@@ -2,8 +2,8 @@
 let canvas = document.querySelector("canvas")
 ;
 //Configuración de los parámetros del canvas.
-canvas.width = 750;
-canvas.height = 750;
+canvas.width = 450;
+canvas.height = 450;
 var ctx = canvas.getContext('2d');
 
 //Variable para los pedidos
@@ -15,58 +15,150 @@ let pendPack = 0;
     Canvas para el mapa
     :D
 */
-// Parámetros de las líneas
-var colour = '';
-var startPoint = (0, 0);
-var endPoint = (0, 0);
 
-// Cargando las imagenes del mapa
-function loadMap(){
-    var mapImage = document.getElementById('mapImage');
-    //var pinIcon = document.getElementById('mapPin');
+var mouse = {
 
-    ctx.drawImage(mapImage, 0, 0);
+    x: undefined,
+    y: undefined,
+    click: false
 }
 
-/*
-    Formulario de entrega y botones
-    y los Pop Ups
-*/
-//Se le asigna el texto que va a llevar el label.
-document.getElementById("totalPackages").innerHTML = "Numero de paquetes solicitados: " + reqPack + "<br>" +
-    "Numero de paquetes entregados: " + deliPack + "<br>" + "Número de paquetes pendientes: " + pendPack;
+function Map(){
 
-//Se asocian las las variables con los elementos pertnecientes al HTML.
-var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-    overlay = document.getElementById('overlay'),
-    popup = document.getElementById('popup'),
-    btnCerrarPopup = document.getElementById('btn-cerrar-popup'),
-    btnAvailableR= document.getElementById("btnAvailabeR");
+    var mapImage = new Image();
+    mapImage.src = "./images/map-template.png";
 
-//Función que hace visible el Pop Up
-btnAbrirPopup.addEventListener('click', function () {
-    overlay.classList.add('active');
-    popup.classList.add('active');
+    this.draw = function(){
+
+        ctx.drawImage(mapImage, 0, 0);
+    }
+
+    this.draw();
+}
+
+function Marker(x, y){
+    
+    this.x = x;
+    this.y = y;
+    this.initialy = y;
+
+    this.pinImage = new Image();
+    this.pinImage.src = "./images/marker.png";
+
+    this.draw = function(){
+
+        ctx.drawImage(this.pinImage, this.x, this.y);
+    }
+
+    this.update = function() {
+
+        if (mouse.x > this.x && mouse.x < this.x + 75 && mouse.y > this.initialy && mouse.y < this.initialy + 75){
+
+            ctx.fillStyle = 'rgba(75, 100, 170, .6)'
+            ctx.fillRect(this.x, this.initialy, 75, 75);
+
+            if(this.y > this.initialy - 10){
+
+                this.y -= 2;
+            }
+
+            if(mouse.click){
+                console.log('this pin has been clicked!');
+                mouse.click = false;
+            }
+        }
+        else{
+
+            this.y = this.initialy;
+        }
+
+        this.draw();
+    }
+}
+
+var pinArray = [];
+
+for(var i = 0; i < 4; i++){
+
+    for(var j = 0; j < 4; j++){
+
+        pinArray.push(new Marker(((200 * j) + 75)/2, ((200 * i) + 75)/2));
+    }
+}
+
+function mapRendering(){
+
+    requestAnimationFrame(mapRendering);
+    
+    ctx.clearRect(0, 0, 900, 900);
+    var map = new Map();
+
+    for(var i = 0; i < pinArray.length; i++){
+
+        pinArray[i].update();
+    }
+}
+
+window.addEventListener('mousemove', function(event){
+
+    var rect = canvas.getBoundingClientRect();
+
+    mouse.x = event.clientX - rect.left;
+    mouse.y = event.clientY - rect.top;
+
+    //console.log(mouse);
 });
 
-//Función que esconde el Pop Up
-btnCerrarPopup.addEventListener('click', function (e) {
-    e.preventDefault();
-    overlay.classList.remove('active');
-    popup.classList.remove('active');
-});
-//Función de prueba cuando se le da al boton de ver Rutas disponibles.
-btnAvailableR.addEventListener('click',function(){
-    alert("Se cerró");
-});
+window.addEventListener('mousedown', function(event){
 
-//Función de prueba cuando se le da al boton de ver Rutas disponibles.
-btnAvailableR.addEventListener('click',function(){
-    alert("Se cerró");
-});
+    if(event.button == 0){
 
-
-setTimeout(() => {
-
-
+        mouse.click = true;
+        console.log('there\'s a click :3' + mouse.click);
+    }
+    
 })
+
+window.addEventListener('mouseup', function(event){
+
+    if(event.button == 0) mouse.click = false;
+})
+
+// /*
+//     Formulario de entrega y botones
+//     y los Pop Ups
+// */
+// //Se le asigna el texto que va a llevar el label.
+// document.getElementById("totalPackages").innerHTML = "Numero de paquetes solicitados: " + reqPack + "<br>" +
+//     "Numero de paquetes entregados: " + deliPack + "<br>" + "Número de paquetes pendientes: " + pendPack;
+
+// //Se asocian las las variables con los elementos pertnecientes al HTML.
+// var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
+//     overlay = document.getElementById('overlay'),
+//     popup = document.getElementById('popup'),
+//     btnCerrarPopup = document.getElementById('btn-cerrar-popup'),
+//     btnAvailableR= document.getElementById("btnAvailabeR");
+
+// //Función que hace visible el Pop Up
+// btnAbrirPopup.addEventListener('click', function () {
+//     overlay.classList.add('active');
+//     popup.classList.add('active');
+// });
+
+// //Función que esconde el Pop Up
+// btnCerrarPopup.addEventListener('click', function (e) {
+//     e.preventDefault();
+//     overlay.classList.remove('active');
+//     popup.classList.remove('active');
+// });
+// //Función de prueba cuando se le da al boton de ver Rutas disponibles.
+// btnAvailableR.addEventListener('click',function(){
+//     alert("Se cerró");
+// });
+
+// //Función de prueba cuando se le da al boton de ver Rutas disponibles.
+// btnAvailableR.addEventListener('click',function(){
+//     alert("Se cerró");
+// });
+
+mapRendering();
